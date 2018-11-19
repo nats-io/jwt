2331ee4ee7e4407d3e51530fc506913c5c1ca7a0
Derek Collison <derek@nats.io>
2018-11-19 12:13:30 -0800

    json decorator
    Signed-off-by: Derek Collison <derek@nats.io>

--- exports.go
+++ exports.go
@@ -9,7 +9,7 @@ type Export struct {
 	Name     string     `json:"name,omitempty"`
 	Subject  Subject    `json:"subject,omitempty"`
 	Type     ExportType `json:"type,omitempty"`
-	TokenReq bool
+	TokenReq bool       `json:"token_req,omitempty"`
 }
 
 // IsService returns true if an export is for a service
