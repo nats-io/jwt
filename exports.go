2007389fa81603b3a247815d7e3a7bd281974ea6
Matthias Hanel <mh@synadia.com>
2020-06-02 14:59:56 -0400

    Moving change from v1 to v2
    Signed-off-by: Matthias Hanel <mh@synadia.com>

--- exports.go
+++ exports.go
@@ -17,7 +17,6 @@ package jwt
 
 import (
 	"fmt"
-	"strings"
 	"time"
 )
 
@@ -126,23 +125,6 @@ func (e *Export) Validate(vr *ValidationResults) {
 		e.Latency.Validate(vr)
 	}
 	e.Subject.Validate(vr)
-
-	if e.AccountTokenPosition > 0 {
-		if !e.Subject.HasWildCards() {
-			vr.AddError("Account Token Position can only be used with wildcard subjects: %s", e.Subject)
-		} else {
-			subj := string(e.Subject)
-			token := strings.Split(subj, ".")
-			tkCnt := uint(len(token))
-			if e.AccountTokenPosition > tkCnt {
-				vr.AddError("Account Token Position %d exceeds length of subject '%s'",
-					e.AccountTokenPosition, e.Subject)
-			} else if tk := token[e.AccountTokenPosition-1]; tk != "*" {
-				vr.AddError("Account Token Position %d matches '%s' but must match a * in: %s",
-					e.AccountTokenPosition, tk, e.Subject)
-			}
-		}
-	}
 }
 
 // Revoke enters a revocation by publickey using time.Now().
