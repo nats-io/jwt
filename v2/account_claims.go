4e75b78f9ce161c50e6248d615b8bd1f63af43ee
Matthias Hanel <mh@synadia.com>
2022-01-28 12:25:04 -0500

    Adding support for HA resources
    Signed-off-by: Matthias Hanel <mh@synadia.com>

--- v2/account_claims.go
+++ v2/account_claims.go
@@ -51,6 +51,7 @@ func (n *NatsLimits) IsUnlimited() bool {
 }
 
 type JetStreamLimits struct {
+	HaResources      int64 `json:"ha_resources,omitempty"`       // Max number of bytes high availability resources (streams & consumer). (0 means disabled)
 	MemoryStorage    int64 `json:"mem_storage,omitempty"`        // Max number of bytes stored in memory across all streams. (0 means disabled)
 	DiskStorage      int64 `json:"disk_storage,omitempty"`       // Max number of bytes stored on disk across all streams. (0 means disabled)
 	Streams          int64 `json:"streams,omitempty"`            // Max number of streams
@@ -60,7 +61,7 @@ type JetStreamLimits struct {
 
 // IsUnlimited returns true if all limits are unlimited
 func (j *JetStreamLimits) IsUnlimited() bool {
-	return *j == JetStreamLimits{NoLimit, NoLimit, NoLimit, NoLimit, false}
+	return *j == JetStreamLimits{NoLimit, NoLimit, NoLimit, NoLimit, NoLimit, false}
 }
 
 // OperatorLimits are used to limit access by an account
@@ -189,7 +190,7 @@ func NewAccountClaims(subject string) *AccountClaims {
 	c.Limits = OperatorLimits{
 		NatsLimits{NoLimit, NoLimit, NoLimit},
 		AccountLimits{NoLimit, NoLimit, true, NoLimit, NoLimit},
-		JetStreamLimits{0, 0, 0, 0, false}}
+		JetStreamLimits{0, 0, 0,0, 0, false}}
 	c.Subject = subject
 	c.Mappings = Mapping{}
 	return c
